6e6e0e08bcefd3d5400d112974c0bce6fade15c4
Imdad-Kaisar <ikaisar345@gamil.com>
2024-06-01 14:32:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div  class="h-screen flex items-center justify-center">
+    <h1 className="text-3xl font-bold text-blue-500 underline">
+      Welcome to Miracle Tourism
+    </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
