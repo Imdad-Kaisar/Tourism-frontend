bc4f4bbbc5f8ffeacb35964fde3ab43f7d12f8cb
Imdad-Kaisar <ikaisar345@gamil.com>
2024-06-02 15:16:54 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import Background from './components/Background.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import GettingStartedButton from './components/intro/Intro.jsx';
+import Footer from './components/footer/Footer.jsx';
+import './App.css'
 
 
 const App =() => {
   
   return (
-    <div>
-      <Background />
+    <div className='app'>
+      
+      
+      
+      
+      
       <Header />
       <GettingStartedButton />
+      
+      <Background />
+      <Footer />
+      
+    
       
       
     </div>
